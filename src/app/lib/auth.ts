1768913cebafcf86933031c8254a03a0fa57beb4
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-10-19 23:44:22 +0500

    add: all files for app/lib added

--- src/app/lib/auth.ts
+++ src/app/lib/auth.ts
@@ -6,11 +6,5 @@ import prisma from "./db";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [GitHub({
-    clientId: process.env.GITHUB_ID!,
-    clientSecret: process.env.GITHUB_SECRET!,
-  }), Google({
-    clientId: process.env.GOOGLE_ID!,
-    clientSecret: process.env.GOOGLE_SECRET!
-  })],
+  providers: [GitHub, Google],
 });
